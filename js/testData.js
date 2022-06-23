90a78dbfaca2c9ea167275cd96752d9ebde88bbe
Jed Stout <jedstout@gmail.com>
2022-06-22 22:10:25 -0600

    Simplified functions, displayed selection, working towards file display

--- js/testData.js
+++ js/testData.js
@@ -1,1 +1,17 @@
-var data = {"testing, this thing on??"}
+function previewFile(x) {
+    console.log(x);
+    var x = document.getElementById(x).src()
+    // const content = document.querySelector('.content');
+    // const [file] = document.querySelector('input[type=file]').files;
+    // const reader = new FileReader();
+  
+    // reader.addEventListener("load", () => {
+    //   // this will then display a text file
+    //   content.innerText = reader.result;
+    // }, false);
+  
+    // if (file) {
+    //   reader.readAsText(file);
+    // }
+  }
+  
