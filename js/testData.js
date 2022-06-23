0553d6162363dc6bfbade0d9c1d4edf29cf3f6ab
Jed Stout <jedstout@gmail.com>
2022-06-23 13:52:57 -0600

    Passing x to file preview function

--- js/testData.js
+++ js/testData.js
@@ -1,12 +1,27 @@
 function previewFile(x) {
+  if (x === 'Test_1') {
+    document.getElementById("demo").innerHTML = x;
     console.log(x);
-    var x = document.getElementById(x).src()
+    } else if (x === 'Test_2') {
+    document.getElementById("demo").innerHTML = x;
+    console.log(x);
+    } else if (x === 'Test_3') {
+    document.getElementById("demo").innerHTML = x;
+    console.log(x);
+    } else if (x === 'Test_4') {
+    document.getElementById("demo").innerHTML = x;
+    console.log(x);
+    } else if (x === 'Test_5') {
+    document.getElementById("demo").innerHTML = x;
+    console.log(x);
+    } else {
+      console.log('Error');
+    }
     // const content = document.querySelector('.content');
     // const [file] = document.querySelector('input[type=file]').files;
     // const reader = new FileReader();
   
     // reader.addEventListener("load", () => {
-    //   // this will then display a text file
     //   content.innerText = reader.result;
     // }, false);
   
