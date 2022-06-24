12c172dce8c652d17bf99f365f1e5869b96830e9
Jed Stout <jedstout@gmail.com>
2022-06-23 21:38:44 -0600

    ACH imported and displaying properly, working on parsing data

--- js/testData.js
+++ js/testData.js
@@ -1,32 +1,19 @@
-function previewFile(x) {
-  if (x === 'Test_1') {
-    document.getElementById("demo").innerHTML = x;
-    console.log(x);
-    } else if (x === 'Test_2') {
-    document.getElementById("demo").innerHTML = x;
-    console.log(x);
-    } else if (x === 'Test_3') {
-    document.getElementById("demo").innerHTML = x;
-    console.log(x);
-    } else if (x === 'Test_4') {
-    document.getElementById("demo").innerHTML = x;
-    console.log(x);
-    } else if (x === 'Test_5') {
-    document.getElementById("demo").innerHTML = x;
-    console.log(x);
-    } else {
-      console.log('Error');
-    }
-    // const content = document.querySelector('.content');
-    // const [file] = document.querySelector('input[type=file]').files;
-    // const reader = new FileReader();
-  
-    // reader.addEventListener("load", () => {
-    //   content.innerText = reader.result;
-    // }, false);
-  
-    // if (file) {
-    //   reader.readAsText(file);
-    // }
+function importData() {
+  const content = document.querySelector('.content');
+  const [file] = document.querySelector('input[type=file]').files;
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    content.innerText = reader.result;
+    var achstring = reader.result;
+    stringifyData(achstring);
+  });
+
+  if (file) {
+    reader.readAsText(file);
   }
-  
+}
+
+function stringifyData(achstring) {
+  console.log(achstring);
+}
