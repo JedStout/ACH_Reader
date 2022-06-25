e4411ec17dc050f40047f9572e4c55a89a8ac1ec
Jed Stout <jedstout@gmail.com>
2022-06-25 00:43:18 -0600

    Analyzing ACH strings for variables

--- js/testData.js
+++ js/testData.js
@@ -15,9 +15,24 @@ function importData() {
 }
 
 function analyzeData(achData) {
-  var data = achData.innerText;
-  var textByLine = data.split("\n");
-  console.log(textByLine[0]);
+  var data = achData.innerText,
+      textByLine = data.split("\n"),
+      firstRow = textByLine[0].split(" "),
+      fileHeader = firstRow[0],
+      destination = firstRow[1];
+
+  if (fileHeader === '101') {
+    console.log(fileHeader);
+    if (fileHeader.startsWith('1')) {
+      console.log('Record Type is 1 and ACH will continue processing')
+    }
+    return fileHeader;
+  } else if (destination !== '0') {
+    console.log(destination);
+  } else {
+    console.log(firstRow[2])
+  };
+  
 }
 
 showButton = (results)=> {document.getElementById(results).style.display = "table";}
